5bb2bfa1055ba6cf9291f10b44f37f1fa307d93e
Monis Azeem <83574521+monis07@users.noreply.github.com>
2023-10-12 17:06:07 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,20 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let sum1=0;
+  let sum2=0;
+for(let i=0;i<str1.length;i++){
+  let a=str1.charCodeAt(i);
+  sum1+=a;
+  let b=str2.charCodeAt(i);
+  sum2+=b;
 }
+  if(sum1===sum2)
+    return true;
+  else
+    return false;
+}
+
+console.log(isAnagram('spar','rasp'));
 
 module.exports = isAnagram;
