06ac789bd5f8b48b85458f01650895c7ddd82aff
Monis Azeem <83574521+monis07@users.noreply.github.com>
2023-10-13 16:47:04 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,48 @@
 */
 
 class Todo {
-
+  constructor(list){
+    this.arr=list;
+  }
+  add(todo){
+    this.arr.push(todo);
+    console.log(this.arr);
+  }
+  remove(indexOfTodo){
+    this.arr.splice(indexOfTodo,1);
+    console.log(this.arr);
+  }
+  update(index,updatedTodo){
+    this.arr.splice(index,0,updatedTodo)
+    console.log(this.arr);
+  }
+  getAll(){
+  console.log(this.arr);
+  }
+  get(indexOfTodo){
+    console.log(this.arr[indexOfTodo]);
+  }
+clear(){
+  this.arr=[];
+  console.log(this.arr);
 }
+}
+
+var todo1=[];
+
+
+let obj1=new Todo(todo1);
+
+obj1.add("Breakfast");
+obj1.add("Lunch");
+obj1.add("Dinner");
+obj1.update(2,"Snacks");
+obj1.remove(0);
+obj1.update(0,'Breakfast');
+obj1.get(3);
+obj1.clear();
+
+
+
 
 module.exports = Todo;
