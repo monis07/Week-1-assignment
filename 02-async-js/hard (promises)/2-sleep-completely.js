ebd6b33e4a79041aaea99f66733e37e9463342fe
Monis Azeem <83574521+monis07@users.noreply.github.com>
2023-10-14 10:17:26 +0530

    Update 2-sleep-completely.js

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,11 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds){
+  var time=Date.now();
+  while(Date.now()-time<seconds){
+  }
 }
+console.log("Before sleep");
+sleep(3000);
+console.log("This will be logged after 3 secs");
